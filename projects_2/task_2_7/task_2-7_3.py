1529ae018344b6dee0efe0200a21bacc42d784f7
JuliaStolbova <ulastolbova263@gmail.com>
2026-02-19 15:26:46 +0400

    task_2_7

---
+++ projects_2/task_2_7/task_2-7_3.py
@@ -0,0 +1,10 @@
+seq = ["ATATACGCGTA", "CTTCGGNGGA"]
+
+for sequence in seq:
+    print(sequence)
+    print()  
+    for char in sequence:
+        print(char)
+    print()  
+
+print("Цикл выполнен")
